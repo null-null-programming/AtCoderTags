34ec0190c0fa81b1b27056c301ed91d346368705
nullnull <nullnullprogramming@gmail.com>
2019-11-29 10:42:57 +0900

    add name

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import *
 import json
 import time
+import requests
 
 class Tag(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -26,9 +27,18 @@ def category():
 
 @app.route('/tag_search', methods=['POST'])
 def tag_search():
+    #コンテスト名取得のため、AtCoderProblemsAPIを利用する。
+    get_problem=requests.get('https://kenkoooo.com/atcoder/resources/problems.json')
+    get_problem=get_problem.json()
+
     tagName = request.args.get('tagName')
     problems = db.session.query(problem_tag).filter_by(first_tag=tagName)
-    return render_template('tag_search.html', tagName=tagName,problems=problems)
+
+    dict={}
+    for problem in get_problem:
+        dict[str(problem['id'])]=problem
+
+    return render_template('tag_search.html', tagName=tagName,problems=problems,dict=dict)
 
 @app.cli.command('initdb')
 def initdb_command():
