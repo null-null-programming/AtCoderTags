45c43479717918ec1427ccd91d8b752a4e90d5d0
nullnull <nullnullprogramming@gmail.com>
2019-12-09 20:29:52 +0900

    fix

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ service = OAuth1Service(
     consumer_key=os.environ["consumer_key"],
     consumer_secret=os.environ["consumer_secret"],
     request_token_url="https://api.twitter.com/oauth/request_token",
-    authorize_url="https://api.twitter.com/oauth/authenticate",
+    authorize_url="https://api.twitter.com/oauth/authorize",
     access_token_url="https://api.twitter.com/oauth/access_token",
     base_url="https://api.twitter.com/1.1/",
 )
